78cc755a5f47762fc58c418e0cc99a103b957fad
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-04-27 10:14:15 +0500

    updated version

--- wordcounter.js
+++ wordcounter.js
@@ -1,10 +1,47 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-let question = await inquirer.prompt([{
-        name: "Sentence",
-        type: "input",
-        message: chalk.redBright("Enter your Sentence to count the words?"),
-    }]);
-const countWords = question.Sentence.trim().split(" ");
-console.log(countWords);
-console.log(chalk.blue(`"Your sentence word count is ${countWords.length} words"`));
+async function exitanimation(text) {
+    for (let char of text) {
+        process.stdout.write(char);
+        await new Promise((resolve) => setTimeout(resolve, 10));
+    }
+}
+let condition = true;
+while (condition) {
+    let question = await inquirer.prompt([
+        {
+            name: "Sentence",
+            type: "input",
+            message: chalk.blue("Enter your Sentence to count the words?"),
+            validate: function (input) {
+                if (input.trim() === "") {
+                    return chalk.red.bold("Please write your sentence!!");
+                }
+                return true;
+            },
+        },
+    ]);
+    const countWords = question.Sentence.trim().split(" ");
+    console.log(countWords);
+    let wordCountmessage;
+    if (countWords.length === 1) {
+        wordCountmessage = "1 word";
+    }
+    else {
+        wordCountmessage = `${countWords.length} words`;
+    }
+    console.log(chalk.gray.bold(`"Your sentence word count is ${wordCountmessage} "`));
+    const continuecon = await inquirer.prompt({
+        name: "question",
+        message: "Do you want to continue?",
+        type: "confirm",
+        default: true
+    });
+    condition = continuecon.question;
+    if (!condition) {
+        await exitanimation(chalk.yellow("-----------------------------------------------"));
+        await exitanimation(chalk.yellow.bold.italic(`\nThank you for using word counter App!\n\t Ayaz Ahmed\n`));
+        await exitanimation(chalk.yellow("-----------------------------------------------\n"));
+    }
+}
--- wordcounter.ts
+++ wordcounter.ts
@@ -2,15 +2,72 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-let question: {Sentence : string } =  await inquirer.prompt([{
-name: "Sentence",
-type: "input",
-message: chalk.redBright("Enter your Sentence to count the words?"),
+async function exitanimation(text: string) {
+    for (let char of text) {
+      process.stdout.write(char);
+  
+      await new Promise((resolve) => setTimeout(resolve, 10));
+    }
+  }
+  
 
-}]);
+
+let condition = true
+
+while(condition) {
+
+let question: { Sentence: string } = await inquirer.prompt([
+  {
+    name: "Sentence",
+    type: "input",
+    message: chalk.blue("Enter your Sentence to count the words?"),
+    validate: function (input) {
+      if (input.trim() === "") {
+        return chalk.red.bold("Please write your sentence!!");
+      }
+      return true;
+    },
+  },
+]);
 
 const countWords = question.Sentence.trim().split(" ");
 
 console.log(countWords);
 
-console.log(chalk.blue(`"Your sentence word count is ${countWords.length} words"`));
+let wordCountmessage;
+
+if(countWords.length === 1){wordCountmessage = "1 word";}
+
+else {wordCountmessage = `${countWords.length} words`;}
+
+
+console.log(
+  chalk.gray.bold(`"Your sentence word count is ${wordCountmessage} "`)
+);
+
+const continuecon = await inquirer.prompt({
+name: "question",
+message: "Do you want to continue?",
+type: "confirm",
+default: true
+
+});
+
+condition = continuecon.question;
+
+if (!condition) {
+    await exitanimation(
+      chalk.yellow("-----------------------------------------------")
+    );
+    await exitanimation(
+      chalk.yellow.bold.italic(
+        `\nThank you for using word counter App!\n\t Ayaz Ahmed\n`
+      )
+    );
+    await exitanimation(
+      chalk.yellow("-----------------------------------------------\n")
+    );
+  }
+
+
+}
